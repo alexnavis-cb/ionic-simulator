a215f332163e1c93c54916b1e6df45f9b4e69dce
Alex Lawrence <alexnavis85@gmail.com>
2023-04-17 23:23:41 +0530

    ionic simulate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import MessageListItem from '../components/MessageListItem';
 import { useState } from 'react';
 import { Message, getMessages } from '../data/messages';
 import {
+  IonButton,
   IonContent,
   IonHeader,
   IonList,
@@ -12,6 +13,7 @@ import {
   IonToolbar,
   useIonViewWillEnter
 } from '@ionic/react';
+import { InAppBrowser } from "@ionic-native/in-app-browser";
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -28,30 +30,78 @@ const Home: React.FC = () => {
       e.detail.complete();
     }, 3000);
   };
+  const launchDigio = () => {
+    const browserObjectOptions = ["hidenavigationbuttons=yes",
+      "hidden=no",
+      "location=yes",
+      "hideurlbar=yes",
+      "clearsessioncache=yes",
+      "clearcache=yes",
+      "hardwareback=no",
+      "useWideViewPort=no",
+      "lefttoright=yes",
+      "closebuttoncaption=Cancel",
+      "zoom=no",
+      /**ios*/
+      "hidespinner=no",
+      "toolbar=yes",
+      "toolbarposition=top",
+      "enableViewportScale=no",
+      "enableViewportScale=yes",];
+
+    const options = {
+      callback: () => { },
+      "is_iframe": true,
+    }
+    var url = URL.createObjectURL(
+      new Blob(
+        [
+          `<!DOCTYPE html>
+   <html lang="en">
+     <head>
+       <meta charset="utf-8" />
+       <title>Digio Test</title>
+       <script src="https://app.digio.in/sdk/v8/digio.js"></script>
+     </head>
+   
+     <body>
+     <script>
+     let invoiceID = 'ENA230417153546646GMKWT2EE5MULAP'
+     let customerId = 'nithiyashri.s@habile.in'
+     const options = ${JSON.stringify(options)}
+     
+        let digio = new window.Digio(options);
+        digio.init();
+        digio.submit(invoiceID, customerId);
+    </script>
+     </body>
+   </html>`,
+        ],
+        { type: "text/html" }
+      )
+    );
+
+    console.log("ionic")
+    const browserRef = InAppBrowser.create(
+      url,
+      "_self",
+      browserObjectOptions.join()
+    );
+  }
 
   return (
     <IonPage id="home-page">
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Inbox</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonRefresher slot="fixed" onIonRefresh={refresh}>
-          <IonRefresherContent></IonRefresherContent>
-        </IonRefresher>
-
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">
-              Inbox
-            </IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
-        <IonList>
+        {/* <IonList>
           {messages.map(m => <MessageListItem key={m.id} message={m} />)}
-        </IonList>
+        </IonList> */}
+        <IonButton>
+        <div className="App">
+          <header className="App-header">
+            <button onClick={() => launchDigio()} >Launch Digio</button>
+          </header>
+        </div>
+        </IonButton>
       </IonContent>
     </IonPage>
   );
